93c0365b7d3bd1e0021d26e5978139ed2e9a7a9d
Iago Laguna <iagolaguna.83@gmail.com>
2018-12-03 22:29:26 -0200

    refactor

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import {
+  search,
+  searchArtists,
+  searchAlbums,
+  searchPlaylists,
+  searchTracks,
+} from './search';
+
+import {
+  getAlbum,
+  getAlbums,
+  getAlbumTracks,
+} from './album';
+
+
+module.exports = {
+  search,
+  searchArtists,
+  searchAlbums,
+  searchPlaylists,
+  searchTracks,
+  getAlbum,
+  getAlbums,
+  getAlbumTracks,
+};
--- tests/search.spec.js
+++ tests/search.spec.js
@@ -7,7 +7,7 @@ chai.use(sinonChai);
 
 global.fetch = require('node-fetch');
 
-import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/search';
 
 describe('Spotify Wrapper', () => {
   let stubedFetch;
