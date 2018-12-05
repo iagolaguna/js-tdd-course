dec25f028597e135a77e3a07f81c307f784d61e8
Iago Laguna <iagolaguna.83@gmail.com>
2018-12-04 22:29:40 -0200

    ajuste eslint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import toJSON from './utils';
 import { API_URL } from './config';
 
 export default class SpotifyWrapper {
-  constructor (options) {
+  constructor(options) {
     this.apiURL = options.apiURL || API_URL;
     this.token = options.token;
     this.album = album.bind(this)();
